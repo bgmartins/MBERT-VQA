8de59e80a1e7c50630227c57dfc554c90c17da5b
Bruno Emanuel da Graça Martins <bgmartins@g03.hlt.inesc-id.pt>
2021-12-30 17:27:01 +0000

    Minor changes.

--- execute.sh
+++ execute.sh
@@ -1,18 +1,21 @@
 #!/bin/sh
-rm -rf datasets test-results
-mkdir datasets
-cd datasets
-wget https://cloud.sylvainlobry.com/s/4Qg5AXX8YfCswmX/download
-unzip download
-rm -rf download
-cd RSVQA_LR
-unzip Images_LR.zip
-rm -rf Images_LR.zip
-cd ..
-wget https://cloud.sylvainlobry.com/s/f7NpYQKqx4bZStx/download
-unzip download
-rm -rf download
-cd ..
+#rm -rf datasets test-results
+#mkdir test-results
+#mkdir datasets
+#cd datasets
+#wget https://cloud.sylvainlobry.com/s/4Qg5AXX8YfCswmX/download
+#unzip download
+#rm -rf download
+#cd RSVQA_LR
+#unzip Images_LR.zip
+#rm -rf Images_LR.zip
+#cd ..
+#wget https://cloud.sylvainlobry.com/s/f7NpYQKqx4bZStx/download
+#unzip download
+#rm -rf download
+#cd ..
 
-mkdir test-results
-python rsvqa/train.py --run_name='vqa_run_name' --cnn_encoder='tf_efficientnetv2_m' --transformer_model='realformer' --data_dir="RSVQA_LR" --use_pretrained --model_dir='path_to_pretrained_model' --batch_size=16 --num_vis=5 --hidden_size=768 --num_workers=16 --save_dir="test-results" --loss='ASLSingleLabel' --epochs=100
+cd preprocess
+python3 rsvqa_lr_data.py
+cd ..
+python3 rsvqa/train.py --run_name='vqa_run_name' --cnn_encoder='tf_efficientnetv2_m' --transformer_model='realformer' --data_dir="datasets/RSVQA_LR" --use_pretrained --model_dir='path_to_pretrained_model' --batch_size=16 --num_vis=5 --hidden_size=768 --num_workers=16 --save_dir="test-results" --loss='ASLSingleLabel' --epochs=100
