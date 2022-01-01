d0be7e9ef5fac7b09cfd000d7ea686a237ff03e8
bgmartins <bgmartins@gmail.com>
2022-01-01 15:34:04 +0000

    Minor changes.

--- execute.sh
+++ execute.sh
@@ -6,37 +6,59 @@
 #mkdir datasets
 #mkdir trained_model
 
-cd datasets
+#cd datasets
 #wget https://cloud.sylvainlobry.com/s/4Qg5AXX8YfCswmX/download
 #unzip download
 #rm -rf download
 #cd RSVQA_LR
 #unzip Images_LR.zip
 #rm -rf Images_LR.zip
-#cd ..
+#cd ../..
+
+#cd datasets
 #wget https://cloud.sylvainlobry.com/s/f7NpYQKqx4bZStx/download
 #unzip download
 #rm -rf download
+#cd ..
 
+cd datasets
 mkdir RSVQA_BEN
-cd RSVQA_BE
-wget "https://zenodo.org/record/5084904/files/Images.zip?download=1"
-wget "https://zenodo.org/record/5084904/files/Images.zip?download=1"
-wget "https://zenodo.org/record/5084904/files/RSVQAxBEN_split_test_answers.json?download=1"
-wget "https://zenodo.org/record/5084904/files/RSVQAxBEN_split_test_images.json?download=1"
-wget "https://zenodo.org/record/5084904/files/RSVQAxBEN_split_test_questions.json?download=1"
-wget "https://zenodo.org/record/5084904/files/RSVQAxBEN_split_train_answers.json?download=1"
-wget "https://zenodo.org/record/5084904/files/RSVQAxBEN_split_train_images.json?download=1"
-wget "https://zenodo.org/record/5084904/files/RSVQAxBEN_split_train_questions.json?download=1"
-wget "https://zenodo.org/record/5084904/files/RSVQAxBEN_split_val_answers.json?download=1"
-wget "https://zenodo.org/record/5084904/files/RSVQAxBEN_split_val_images.json?download=1"
-wget "https://zenodo.org/record/5084904/files/RSVQAxBEN_split_val_questions.json?download=1"
-cd ..
-cd ..
+cd RSVQA_BEN
+#wget "https://zenodo.org/record/5084904/files/Images.zip?download=1"
+unzip "Images.zip?download=1"
+rm -rf "Images.zip?download=1"
+#wget "https://zenodo.org/record/5084904/files/RSVQAxBEN_split_test_answers.json?download=1"
+#mv "RSVQAxBEN_split_test_answers.json?download=1" RSVQAxBEN_split_test_answers.json
+#wget "https://zenodo.org/record/5084904/files/RSVQAxBEN_split_test_images.json?download=1"
+#mv "RSVQAxBEN_split_test_images.json?download=1" RSVQAxBEN_split_test_images.json
+#wget "https://zenodo.org/record/5084904/files/RSVQAxBEN_split_test_questions.json?download=1"
+#mv "RSVQAxBEN_split_test_questions.json?download=1" RSVQAxBEN_split_test_questions.json
+#wget "https://zenodo.org/record/5084904/files/RSVQAxBEN_split_train_answers.json?download=1"
+#mv "RSVQAxBEN_split_train_answers.json?download=1" RSVQAxBEN_split_train_answers.json
+#wget "https://zenodo.org/record/5084904/files/RSVQAxBEN_split_train_images.json?download=1"
+#mv "RSVQAxBEN_split_train_images.json?download=1" RSVQAxBEN_split_train_images.json
+#wget "https://zenodo.org/record/5084904/files/RSVQAxBEN_split_train_questions.json?download=1"
+#mv "RSVQAxBEN_split_train_questions.json?download=1" RSVQAxBEN_split_train_questions.json
+#wget "https://zenodo.org/record/5084904/files/RSVQAxBEN_split_val_answers.json?download=1"
+#mv "RSVQAxBEN_split_val_answers.json?download=1" RSVQAxBEN_split_val_answers.json
+#wget "https://zenodo.org/record/5084904/files/RSVQAxBEN_split_val_images.json?download=1"
+#mv "RSVQAxBEN_split_val_images.json?download=1" RSVQAxBEN_split_val_images.json
+#wget "https://zenodo.org/record/5084904/files/RSVQAxBEN_split_val_questions.json?download=1"
+#mv "RSVQAxBEN_split_val_questions.json?download=1" RSVQAxBEN_split_val_questions.json
+cd ../..
+
+#cd datasets
+#wget https://github.com/spectralpublic/RSIVQA/archive/refs/heads/main.zip
+#unzip main.zip
+#mv RSIVQA-main/RSIVQA .
+#rm -rf main.zip RSIVQA-main
+#cd ..
+
 cd preprocess
 #python3 rsvqa_lr_data.py
 #python3 rsvqa_hr_data.py
 python3 rsvqa_ben_data.py
+python3 rsvqa_rsivqa_data.py
 cd ..
 
 PYTHONPATH=. python3 rsvqa/train.py --run_name='vqa_run_name' --cnn_encoder='tf_efficientnetv2_m' --transformer_model='realformer' --data_dir="datasets/RSVQA_LR" --model_dir='trained-model' --batch_size=4 --num_vis=5 --hidden_size=768 --num_workers=16 --save_dir="test-results" --loss='ASLSingleLabel' --epochs=100
---
+++ preprocess/rsvqa_rsivqa_data.py
@@ -0,0 +1,60 @@
+import pandas as pd
+import json
+import os
+import glob
+from PIL import Image
+
+data_dir = os.path.join('..', 'datasets/RSVQA_BEN')
+
+train_data_images = json.load(open(os.path.join(data_dir, 'RSVQAxBEN_split_train_images.json')))["images"]
+train_data_questions = json.load(open(os.path.join(data_dir, 'RSVQAxBEN_split_train_questions.json')))["questions"]
+train_data_answers = json.load(open(os.path.join(data_dir, 'RSVQAxBEN_split_train_answers.json')))["answers"]
+
+test_data_images = json.load(open(os.path.join(data_dir, 'RSVQAxBEN_split_test_images.json')))["images"]
+test_data_questions = json.load(open(os.path.join(data_dir, 'RSVQAxBEN_split_test_questions.json')))["questions"]
+test_data_answers = json.load(open(os.path.join(data_dir, 'RSVQAxBEN_split_test_answers.json')))["answers"]
+
+val_data_images = json.load(open(os.path.join(data_dir, 'RSVQAxBEN_split_val_images.json')))["images"]
+val_data_questions = json.load(open(os.path.join(data_dir, 'RSVQAxBEN_split_val_questions.json')))["questions"]
+val_data_answers = json.load(open(os.path.join(data_dir, 'RSVQAxBEN_split_val_answers.json')))["answers"]
+
+train_data_images = [x for x in train_data_images if x['active'] == True]
+train_data_questions = [x for x in train_data_questions if x['active'] == True]
+train_data_answers = [x for x in train_data_answers if x['active'] == True]
+
+test_data_images = [x for x in test_data_images if x['active'] == True]
+test_data_questions = [x for x in test_data_questions if x['active'] == True]
+test_data_answers = [x for x in test_data_answers if x['active'] == True]
+
+val_data_images = [x for x in val_data_images if x['active'] == True]
+val_data_questions = [x for x in val_data_questions if x['active'] == True]
+val_data_answers = [x for x in val_data_answers if x['active'] == True]
+
+train_df = pd.DataFrame(train_data_questions)
+train_df = train_df.merge(pd.DataFrame(train_data_answers), how='inner', suffixes=('_1', '_2'), left_on = 'id', right_on = 'question_id')
+train_df = train_df.drop(['id_1', 'id_2','date_added_1','date_added_2','people_id_1','people_id_2','active_1','active_2','question_id','answers_ids'], axis=1)
+train_df = train_df.rename(columns={"type": "category"})
+train_df.insert(0, 'mode', 'train')
+
+test_df = pd.DataFrame(test_data_questions)
+test_df = test_df.merge(pd.DataFrame(test_data_answers), how='inner', suffixes=('_1', '_2'), left_on = 'id', right_on = 'question_id')
+test_df = test_df.drop(['id_1', 'id_2','date_added_1','date_added_2','people_id_1','people_id_2','active_1','active_2','question_id','answers_ids'], axis=1)
+test_df = test_df.rename(columns={"type": "category"})
+test_df.insert(0, 'mode', 'test')
+
+val_df = pd.DataFrame(val_data_questions)
+val_df = val_df.merge(pd.DataFrame(val_data_answers), how='inner', suffixes=('_1', '_2'), left_on = 'id', right_on = 'question_id')
+val_df = val_df.drop(['id_1', 'id_2','date_added_1','date_added_2','people_id_1','people_id_2','active_1','active_2','question_id','answers_ids'], axis=1)
+val_df = val_df.rename(columns={"type": "category"})
+val_df.insert(0, 'mode', 'val')
+
+for aux in glob.glob(os.path.join(data_dir, 'Images_BEN') + "/*.tif"):
+    im = Image.open(aux)
+    out = im.convert("RGB")
+    out.save(aux[:-3] + "jpg", "JPEG", quality=100)    
+    print(aux)
+
+train_df.to_csv(os.path.join(data_dir, 'traindf.csv'), index=False)
+val_df.to_csv(os.path.join(data_dir, 'valdf.csv'), index=False)
+test_df.to_csv(os.path.join(data_dir, 'testdf.csv'), index=False)
+
